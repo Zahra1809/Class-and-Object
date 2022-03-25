0f67e569b68e9d8266c633004afd37efa96b6d88
Zahra1809 <zahragil1809@gmail.com>
2022-03-25 15:02:16 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_dan_Object_Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+            Taxi taxi = new Class_dan_Object_Taxi.Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriveName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
